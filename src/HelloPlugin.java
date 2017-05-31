c6f5253a48d8e7c3756398948a6f567d53177026
javatlacati <scherzo_16@hotmail.com>
2017-05-31 01:51:29 -0500

    Actualización para correr con IDEA 171.442

--- src/HelloPlugin.java
+++ src/HelloPlugin.java
@@ -1,6 +1,5 @@
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
-import com.intellij.openapi.actionSystem.DataKeys;
 import com.intellij.openapi.ui.MessageType;
 import com.intellij.openapi.ui.popup.Balloon;
 import com.intellij.openapi.ui.popup.JBPopupFactory;
@@ -15,10 +14,10 @@ public class HelloPlugin extends AnAction {
 
     @Override
     public void actionPerformed(AnActionEvent anActionEvent) {
-//        StatusBar statusBar = WindowManager.getInstance()
-//                .getStatusBar(anActionEvent.getProject());
         StatusBar statusBar = WindowManager.getInstance()
-                .getStatusBar(DataKeys.PROJECT.getData(anActionEvent.getProject().getDataContext()));
+                .getStatusBar(anActionEvent.getProject());
+//        StatusBar statusBar = WindowManager.getInstance()
+//                .getStatusBar(DataKeys.PROJECT.getData(anActionEvent.getProject().getDataContext()));
         JBPopupFactory.getInstance()
                 .createHtmlTextBalloonBuilder("Hola Mundo!", MessageType.INFO, null)
                 .setFadeoutTime(7500)
